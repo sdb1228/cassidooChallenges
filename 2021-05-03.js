f82ab884093d9ebc3c23e8fa5b96fc14a813c1de
Steven Burnett <stevendburnett@gmail.com>
2021-05-03 22:09:52 -0600

    may 3rd 2021 challenge

---
+++ 2021-05-03.js
@@ -0,0 +1,9 @@
+const roundToZero = (number) => Math.trunc(number)
+
+
+console.log(roundToZero(1.7))
+console.log(roundToZero(-2.1))
+console.log(roundToZero(500.4))
+console.log(roundToZero(-369.5))
+console.log(roundToZero(150))
+console.log(roundToZero(-350))
